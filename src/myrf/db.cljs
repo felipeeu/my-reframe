bb267880e47a5e05294f730321a3987911a3ecad
felipe.domingues <felipe.domingues@calix.com>
2022-10-19 18:35:15 -0300

    Add event for register cart added

--- src/myrf/db.cljs
+++ src/myrf/db.cljs
@@ -6,22 +6,27 @@
                     :inventory 10
                     :image "no-image.png"
                     :id "1"
-                    :price 20}
+                    :price 20
+                    :cart-added false}
                "2" {:name "mush mush"
                     :quantity 0
                     :inventory 15
                     :image "no-image.png"
                     :id "2"
-                    :price 14.8}
+                    :price 14.8
+                    :cart-added false}
                "3"  {:name "sensorial tube"
                      :quantity 0
                      :inventory 6
                      :image "no-image.png"
                      :id "3"
-                     :price 10.2}
+                     :price 10.2
+                     :cart-added false}
                "4" {:name "peanut"
                     :quantity 0
                     :inventory 8
                     :image "no-image.png"
                     :id "4"
-                    :price 15.22}}})
+                    :price 15.22
+                    :cart-added false}}
+   :cart {}})
--- src/myrf/events.cljs
+++ src/myrf/events.cljs
@@ -17,3 +17,18 @@
  ::reset-quantity
  (fn [db [_ product-info]]
    (assoc-in db [:products product-info :quantity] 0)))
+
+(re-frame/reg-event-db
+ ::register-cart-added
+ (fn [db [_ product-info]]
+   (assoc-in db [:products product-info :cart-added] true)))
+
+(re-frame/reg-event-fx
+ ::add-to-cart
+ (fn [{:keys [db]} [_ product-info quantity]]
+   (let [cart-db  {product-info {:quantity quantity}}]
+     (if (> quantity 0)
+       {:db  (assoc db :cart
+                    (conj  (:cart db) cart-db))
+        :fx [[:dispatch [::register-cart-added product-info]]]}
+       nil))))
