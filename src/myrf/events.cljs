7d9e306986925d92978301d62c6f8eccba6f3d7f
felipe.domingues <felipe.domingues@calix.com>
2022-10-24 15:23:28 -0300

    add event to open modal

--- src/myrf/events.cljs
+++ src/myrf/events.cljs
@@ -1,7 +1,9 @@
 (ns myrf.events
   (:require
    [re-frame.core :as re-frame]
-   [myrf.db :as db]))
+   [myrf.db :as db]
+  ;;  [day8.re-frame.tracing :refer-macros [fn-traced]]
+   ))
 
 (re-frame/reg-event-db
  ::initialize-db
@@ -19,16 +21,31 @@
    (assoc-in db [:products product-info :quantity] 0)))
 
 (re-frame/reg-event-db
- ::register-cart-added
- (fn [db [_ product-info]]
-   (assoc-in db [:products product-info :cart-added] true)))
+ ::register-cart-status
+ (fn [db [_ product-info status]]
+   (assoc-in db [:products product-info :cart-added] status)))
+
+(re-frame/reg-event-fx
+ ::show-modal
+ (fn [{:keys [db]} [_ product-info]]
+   {:db (assoc-in db [:modal-opened] {:status "is-active" :id product-info})}))
+
 
 (re-frame/reg-event-fx
  ::add-to-cart
  (fn [{:keys [db]} [_ product-info quantity]]
    (let [cart-db  {product-info {:quantity quantity}}]
-     (if (> quantity 0)
-       {:db  (assoc db :cart
-                    (conj  (:cart db) cart-db))
-        :fx [[:dispatch [::register-cart-added product-info]]]}
+     {:db  (assoc db :cart
+                  (conj  (:cart db) cart-db))
+      :fx [[:dispatch [::register-cart-status product-info true]]
+           [:dispatch [::show-modal product-info]]]})))
+
+(re-frame/reg-event-fx
+ ::remove-from-cart
+ (fn [{:keys [db]} [_ product-info quantity]]
+   (let [cart-db  {product-info {:quantity quantity}}]
+     (if (< quantity 0)
+       {:db  (dissoc db :cart
+                     (conj  (:cart db) cart-db))
+        :fx [[:dispatch [::register-cart-status product-info false]]]}
        nil))))
