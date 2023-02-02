8665269069c5b36f741736bbed91df841b7e2707
felipe.domingues <felipe.domingues@calix.com>
2023-02-02 19:28:35 -0300

    improve cart and products UI

--- src/myrf/components/cart.cljs
+++ src/myrf/components/cart.cljs
@@ -1,12 +1,26 @@
 (ns myrf.components.cart
   (:require
+   [myrf.subs :as subs]
    [re-frame.core :as re-frame]
-   [myrf.subs :as subs]))
+   [myrf.components.quantity-selector :refer [change-quantity-buttons]]))
 
 
 (defn cart-component
-  "represent a cart of products"
+  "represent the products there is in a cart"
   [product-id]
   (let [cart-quantity @(re-frame/subscribe [::subs/cart-quantity product-id])
-        name @(re-frame/subscribe [::subs/name product-id])]
-    [:div {:key (str name product-id)} name cart-quantity]))
+        name @(re-frame/subscribe [::subs/name product-id])
+        price @(re-frame/subscribe [::subs/price product-id])
+        total (* cart-quantity price)]
+    [:tr {:key (str name product-id)}
+     [:td
+      [:img {:src "no-image.png"
+             :style {:width "80px"}}]]
+     [:td
+      [:p {:class "is-family-primary"} name]]
+     [:td
+      [:p {:class "is-family-primary"} price]]
+     [:td
+      [:p {:class "is-family-primary"} [change-quantity-buttons cart-quantity product-id "cart-quantity"]]]
+     [:td
+      [:p {:class "is-family-primary"} total]]]))
--- src/myrf/components/product.cljs
+++ src/myrf/components/product.cljs
@@ -1,17 +1,30 @@
 (ns myrf.components.product
   (:require  [re-frame.core :as re-frame]
              [myrf.subs :as subs]
-             [myrf.events :as events]))
+             [myrf.events :as events]
+             [myrf.router :as router]))
+
+(defn add-to-cart-button
+  "A button to add products to cart"
+  [product-id quantity]
+  [:button {:on-click #(re-frame.core/dispatch [::events/add-to-cart product-id quantity])
+            :class "button is-success"
+            :disabled (= quantity 0)} "Add to cart"])
+
 
 (defn product
   "represent a single product"
   [product-id]
   (let [name @(re-frame/subscribe [::subs/name product-id])
-        price @(re-frame/subscribe [::subs/price product-id])
-        quantity  @(re-frame/subscribe [::subs/quantity product-id])]
-    [:div {:key (str name product-id) :class "column"}
-     [:div {:class "box"}
-      [:img {:src "no-image.png" :style {:width 80}}]
-      [:p {:class "is-family-primary"} name]
-      [:p {:class "is-family-code"} (str "$"  price)]
-      [:button {:on-click #(re-frame.core/dispatch [::events/show-modal product-id quantity]) :class "button is-info is-light "} "buy"]]]))
+        price @(re-frame/subscribe [::subs/price product-id])]
+    [:div {:key (str name product-id)
+           :class "column"}
+     [:a {:href (router/url-for :product :product-id product-id)}
+      [:div {:class "box"
+             :on-click #(re-frame/dispatch [::events/select-product product-id])
+             :style {:cursor "pointer"
+                     :background "pink"}}
+       [:img {:src "no-image.png" :style {:width 80}}]
+       [:p {:class "is-family-primary"} name]
+       [:p {:class "is-family-code"} (str "$"  price)]]]]))
+
