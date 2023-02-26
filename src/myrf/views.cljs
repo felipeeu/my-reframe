3b7e03479e3eb52b7c23f291b825354bdb49d5c9
felipe.domingues <felipe.domingues@calix.com>
2023-02-26 15:20:54 -0300

    use partition-all instead partition to map products

--- src/myrf/views.cljs
+++ src/myrf/views.cljs
@@ -1,8 +1,10 @@
 (ns myrf.views
   (:require
-   [myrf.components.cart :refer [cart-component cart-table]]
-   [myrf.components.product :refer [add-to-cart-button product product-information]]
+   [myrf.components.cart :refer [cart-table]]
+   [myrf.components.product :refer [add-to-cart-button product
+                                    product-information]]
    [myrf.components.quantity-selector :refer [quantity-component]]
+   [myrf.events :as events]
    [myrf.router :as router]
    [myrf.subs :as subs]
    [myrf.utils.constants :refer [products-columns]]
@@ -15,7 +17,8 @@
      [:ul
       [:li [:a {:href (router/url-for :home)}  "back"]]
       [:li [:a {:href (router/url-for :login)} "login"]]
-      [:li [:input]]
+      [:li [:input {:on-change (fn [e]
+                                 (re-frame.core/dispatch-sync [::events/filter-by-name (-> e .-target .-value)]))}]]
       (if (empty? cart-product-ids) nil
           [:li {:class "pr-2 mt-05 float-right"}
            [:a {:href (router/url-for :cart)}  "cart"]])]]))
@@ -24,24 +27,35 @@
   []
   [:p "all-purpose store by Felipe Domingues"])
 
-(defn render-products
-  "Render products and pass number of columns as parameters"
-  [columns]
-  (let [ids  @(re-frame/subscribe [::subs/list-products-ids])]
-    [:div
-     (->> ids
-          (map  #(product %))
-          (partition columns)
-          (map (fn [%] [:div {:key (rand-int (count ids))
-                              :class "row pt-2 text-center"} %]))
-          (doall))]))
+(defn split-products-into-columns
+  "split lines of products into column"
+  [product-ids products-columns filtered-id]
+  (let [data-to-display (if (empty? filtered-id)
+                          product-ids
+                          filtered-id)
+        mapped-products (map (fn [%] [:div {:class "w-33 mx-auto"} [product %]])
+                             data-to-display)
+        count-ids (count data-to-display)
+        products-panel (if (< count-ids products-columns)
+                         mapped-products
+                         (partition-all products-columns mapped-products))]
+    products-panel))
 
+
+
+(defn render-products
+  "Render products and split lines into colums"
+  [product-ids products-columns filtered-id]
+  [:div
+   (->> (split-products-into-columns product-ids products-columns filtered-id)
+        (map (fn [%] [:div {:key (random-uuid)
+                            :class "row pt-2 text-center"} %]))
+        (doall))])
 
 (defn render-cart
-  []
-  (let [ids  @(re-frame/subscribe [::subs/list-cart-products-ids])]
-    (if (empty? ids) nil
-        [cart-table ids])))
+  [cart-ids]
+  (if (empty? cart-ids) nil
+      [cart-table cart-ids]))
 
 ;------- product page ------ 
 
@@ -67,11 +81,14 @@
 
 (defn pages
   [page-name]
-  (case page-name
-    :home [render-products products-columns]
-    :cart [render-cart]
-    :product [product-panel]
-    [render-products products-columns]))
+  (let [product-ids  @(re-frame/subscribe [::subs/list-products-ids])
+        cart-ids  @(re-frame/subscribe [::subs/list-cart-products-ids])
+        filtered-id @(re-frame/subscribe [::subs/filtered-data])]
+    (case page-name
+      :home [render-products product-ids products-columns filtered-id]
+      :cart [render-cart cart-ids]
+      :product [product-panel]
+      [render-products product-ids products-columns filtered-id])))
 
 (defn main-panel
   []
